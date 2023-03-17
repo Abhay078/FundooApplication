e46cf96f11743e784be3b48762171838e66d8f9c
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-17 13:24:12 +0530

    [Abhay]-Model class of Updating a note

---
+++ CommonLayer/Model/UpdateNoteModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class UpdateNoteModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+        
+    }
+}
