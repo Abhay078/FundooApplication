94e11ab0d57abdf4a30fd51e2b1a6b8e7399bea4
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-17 13:20:54 +0530

    [Abhay]-Model class of Creating a note

---
+++ CommonLayer/Model/CreateNoteModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class CreateNoteModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Reminder { get; set; }
+        //public string BackgroundColor { get; set; }
+        //public string Image { get; set; }
+        public bool Archived { get; set; } = true;
+        public bool Trash { get; set; } = true;
+        public bool IsPinned { get; set; } = true;
+        
+    }
+}
