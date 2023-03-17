dae14f6ab7cdad39863b832b85a983cea3ee6f7e
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-17 16:32:52 +0530

    [Abhay]-Migration file for Collaborator functionality

---
+++ RepositoryLayer/Migrations/20230317082247_CollabTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class CollabTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collab",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(nullable: false),
+                    NoteId = table.Column<long>(nullable: false),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collab", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Collab_Notes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Notes",
+                        principalColumn: "NoteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Collab_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collab_NoteId",
+                table: "Collab",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collab_UserId",
+                table: "Collab",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Collab");
+        }
+    }
+}
