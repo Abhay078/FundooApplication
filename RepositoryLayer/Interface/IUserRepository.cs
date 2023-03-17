ceaa35eaf6a01e57f21a0c3aaf5ccd90dbeaa92d
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-17 13:33:47 +0530

    [Abhay]-Interface for User Repository

---
+++ RepositoryLayer/Interface/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IUserRepository
+    {
+        public UserEntity UserRegister(UserRegModel model);
+        public UserEntity Login(LoginModel model);
+        public string Generate(string Email, long Userid);
+
+        public IEnumerable<UserEntity> GetAllUser();
+
+        public UserEntity GetUserById(long id);
+
+        public string ForgetPassword(string email);
+
+        public bool ResetPassword(ResetPasswordModel ResetModel,string email);
+    }
+}
