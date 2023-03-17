e4529c3ebdcfb348d5e66160abca3a470581c4ed
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-17 13:33:17 +0530

    [Abhay]-Interface for Notes Repository

---
+++ RepositoryLayer/Interface/INotesRepository.cs
@@ -0,0 +1,25 @@
+﻿using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface INotesRepository
+    {
+        public NotesEntity CreateNote(CreateNoteModel model,long UserId);
+        public IEnumerable<NotesEntity> GetAllNote(long userId);
+
+        public NotesEntity GetNoteById(long id,long UserId);
+        public NotesEntity UpdateNote(UpdateNoteModel model, long id, long UserId);
+        public bool DeleteNote(long id, long UserId);
+        public NotesEntity ArchieveNote(long id, long UserId);
+        public NotesEntity TrashNote(long id, long UserId);
+        public NotesEntity PinNote(long id, long UserId);
+
+        public NotesEntity ColorNote(long Noteid, long UserId, string Color);
+        public NotesEntity Image(IFormFile Image, long Noteid, long UserId);
+    }
+}
