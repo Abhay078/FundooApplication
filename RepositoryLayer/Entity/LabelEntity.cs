e4a796a52491c7d8be75d6c276d78383c706a8e9
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 20:26:29 +0530

    [Abhay]-ModelClass for label

---
+++ RepositoryLayer/Entity/LabelEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class LabelEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long LabelId { get; set; }
+        public long NoteId { get; set; }
+        [ForeignKey("NoteId")]
+        public NotesEntity NotesEntity { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+        public UserEntity UserEntity { get; set; }
+
+        
+        public string LabelName { get; set; }
+
+    }
+}
