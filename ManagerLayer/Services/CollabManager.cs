ee721640d4d2270cae489672c88ccc1ad8abe08e
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 23:22:39 +0530

    [Abhay]-Manager layer logic of collab

---
+++ ManagerLayer/Services/CollabManager.cs
@@ -0,0 +1,34 @@
+﻿using CommonLayer.Model;
+using ManagerLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.FundooDBContext;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Services
+{
+    public class CollabManager:ICollabManager
+    {
+        private readonly ICollabRepository repository;
+        public CollabManager(ICollabRepository repository)
+        {
+            this.repository=repository;
+        }
+        public CollabEntity AddCollab(AddCollabModel model,long UserId)
+        {
+            return repository.AddCollab(model,UserId);
+        }
+        public CollabEntity DeleteCollab(long UserId, string email, long NoteId)
+        {
+            return repository.DeleteCollab(UserId,email,NoteId);
+     
+        }
+        public IEnumerable<CollabEntity> GetAllCollab(long UserId)
+        {
+            return repository.GetAllCollab(UserId);
+        }
+        
+    }
+}
