fdf0f549e65432690aa51a3e613729a2781ff4f7
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-21 00:02:01 +0530

    [Abhay]-User Registration Validation added

--- CommonLayer/Model/UserRegModel.cs
+++ CommonLayer/Model/UserRegModel.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CommonLayer.Model
 {
     public class UserRegModel
     {
+        [RegularExpression(@"[A-Z]{1}[a-z]{1,}")]
         public string FirstName { get; set; }
+        [RegularExpression(@"[A-Z]{1}[a-z]{1,}")]
         public string LastName { get; set; }
+        [RegularExpression(@"^[a-z0-9\w]{3,}@[-a-z0-9]{5,}\.[a-z]{2,5}$")]
         public string Email { get; set; }
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8}")]
         public string Password { get; set; }
     }
 }
