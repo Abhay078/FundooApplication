46b93f96d44b4b3d8c946207ae0794f809ab343d
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 20:27:05 +0530

    [Abhay]-ModelClass for Collab

---
+++ RepositoryLayer/Entity/CollabEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class CollabEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+        public UserEntity UserEntity { get; set; }
+        
+        public long NoteId { get; set; }
+        [ForeignKey("NoteId")]
+        public NotesEntity NotesEntity { get; set; }
+        public string Email { get; set; }
+    }
+}
