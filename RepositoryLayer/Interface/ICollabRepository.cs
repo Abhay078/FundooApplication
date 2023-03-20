fb0f78fd4a03c46b89f7e6ba132c1c8d6ec75733
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 20:32:38 +0530

    [Abhay]-Interface for Repo class of collab

---
+++ RepositoryLayer/Interface/ICollabRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ICollabRepository
+    {
+        public CollabEntity AddCollab(AddCollabModel model,long UserId);
+        public CollabEntity DeleteCollab(long UserId, string email, long NoteId);
+        public IEnumerable<CollabEntity> GetAllCollab(long UserId);
+        
+    }
+}
