d4a9d10bf2daba5ace38aba8ef743bf209362b85
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 23:19:20 +0530

    [Abhay]-Manager service of collaborators

---
+++ ManagerLayer/Interface/ICollabManager.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface ICollabManager
+    {
+        public CollabEntity AddCollab(AddCollabModel model, long UserId);
+        public CollabEntity DeleteCollab(long UserId, string email, long NoteId);
+        public IEnumerable<CollabEntity> GetAllCollab(long UserId);
+    }
+}
