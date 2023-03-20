239bfd230347924b5ff8ac59a76d9f4fd1e3a709
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 23:20:44 +0530

    [Abhay]-Manager service of User

---
+++ ManagerLayer/Interface/IUserManager.cs
@@ -0,0 +1,21 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface IUserManager
+    {
+        public UserEntity UserRegister(UserRegModel model);
+        public UserEntity Login(LoginModel model);
+        public string Generate(string Email, long Userid);
+
+        public IEnumerable<UserEntity> GetAllUser();
+        public UserEntity GetUserById(long id);
+
+        public string ForgetPassword(string email);
+        public bool ResetPassword(ResetPasswordModel ResetModel,string email);
+    }
+}
