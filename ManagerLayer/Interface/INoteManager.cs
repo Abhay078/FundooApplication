e92433e92103dec067e24cae0b00af60b7ee3450
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-20 23:20:20 +0530

    [Abhay]-Manager service of Notes

---
+++ ManagerLayer/Interface/INoteManager.cs
@@ -0,0 +1,29 @@
+﻿using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLayer.Interface
+{
+    public interface INoteManager
+    {
+        public NotesEntity CreateNote(CreateNoteModel model,long userId);
+        public IEnumerable<NotesEntity> GetAllNote(long UserId);
+        public NotesEntity GetNoteById(long id, long UserId);
+        public NotesEntity UpdateNote(UpdateNoteModel model, long id,long UserId);
+        public bool DeleteNote(long id,long UserId);
+        public NotesEntity ArchieveNote(long id, long UserId);
+        public NotesEntity TrashNote(long id, long UserId);
+
+        public NotesEntity PinNote(long id, long UserId);
+        public NotesEntity ColorNote(long Noteid, long UserId, string Color);
+        public NotesEntity Image(IFormFile Image, long Noteid, long UserId);
+
+        public IEnumerable<NotesEntity> RetrieveMatching(string Keyword, long UserId,out int count);
+        public List<NotesEntity> Pagination(string Keyword, int pageNo, int pageSize,long UserId);
+        
+
+    }
+}
