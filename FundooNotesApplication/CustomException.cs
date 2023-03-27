b0b89807d9edb3978b08ba8431303acd83670afd
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-27 13:25:00 +0530

    [Abhay]-added Custom Exception Class

---
+++ FundooNotesApplication/CustomException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepositoryLayer.Services
+{
+    public class CustomException : Exception
+    {
+
+        public CustomException() { }
+        public CustomException(string message) : base(message) { }
+        
+    }
+}
