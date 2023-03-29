9f7f9de82813aa84e00b8d6d11c2806bcd0a6e62
Abhay078 <abhaysrivastava078@gmail.com>
2023-03-29 09:20:46 +0530

    [Abhay]-added consumer microservice for rabbitMQ

--- AddCollabConsumer/Startup.cs
+++ AddCollabConsumer/Startup.cs
@@ -1,19 +1,16 @@
 
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using RepositoryLayer.FundooDBContext;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace AddCollabConsumer
 {
@@ -60,6 +57,8 @@ namespace AddCollabConsumer
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+
 
             app.UseHttpsRedirection();
 
